1077f523bcc2520d023476192da4aca3b115d412
Fadhlan Sulistiyo Hidayat <fadhlansulistiyo@gmail.com>
2024-11-27 19:22:14 +0700

    test: top rated movie test

---
+++ test/presentation/bloc/movie/top_rated/top_rated_movie_bloc_test.dart
@@ -0,0 +1,80 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/domain/entity/movie/movie.dart';
+import 'package:ditonton/domain/usecases/movie/get_top_rated_movies.dart';
+import 'package:ditonton/presentation/bloc/movie/top_rated/top_rated_movie_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:bloc_test/bloc_test.dart';
+import '../../../provider/movie/movie_list_notifier_test.mocks.dart';
+
+@GenerateMocks([GetTopRatedMovies])
+void main() {
+  late TopRatedMovieBloc topRatedMovieBloc;
+  late MockGetTopRatedMovies mockGetTopRatedMovies;
+
+  final tMovieModel = Movie(
+    adult: false,
+    backdropPath: '/muth4OYamXf41G2evdrLEg8d3om.jpg',
+    genreIds: const [14, 28],
+    id: 557,
+    originalTitle: 'Spider-Man',
+    overview:
+        'After being bitten by a genetically altered spider, nerdy high school student Peter Parker is endowed with amazing powers to become the Amazing superhero known as Spider-Man.',
+    popularity: 60.441,
+    posterPath: '/rweIrveL43TaxUN0akQEaAXL6x0.jpg',
+    releaseDate: '2002-05-01',
+    title: 'Spider-Man',
+    video: false,
+    voteAverage: 7.2,
+    voteCount: 13507,
+  );
+
+  final tMovieList = <Movie>[tMovieModel];
+
+  setUp(() {
+    mockGetTopRatedMovies = MockGetTopRatedMovies();
+    topRatedMovieBloc = TopRatedMovieBloc(mockGetTopRatedMovies);
+  });
+
+  test('initial state should be empty', () {
+    expect(topRatedMovieBloc.state, TopRatedMovieEmpty());
+  });
+
+  blocTest<TopRatedMovieBloc, TopRatedMovieState>(
+    'Should emit [Loading, HasData] when data is gotten successfully',
+    build: () {
+      when(mockGetTopRatedMovies.execute())
+          .thenAnswer((_) async => Right(tMovieList));
+      return topRatedMovieBloc;
+    },
+    act: (bloc) => bloc.add(FetchTopRatedMovie()),
+    wait: const Duration(milliseconds: 100),
+    expect: () => [
+      TopRatedMovieLoading(),
+      TopRatedMovieHasData(tMovieList),
+    ],
+    verify: (bloc) {
+      verify(mockGetTopRatedMovies.execute());
+    },
+  );
+
+  blocTest<TopRatedMovieBloc, TopRatedMovieState>(
+    'Should emit [Loading, Error] when get top rated movie is unsuccessful',
+    build: () {
+      when(mockGetTopRatedMovies.execute())
+          .thenAnswer((_) async => Left(ServerFailure('Server Failure')));
+      return topRatedMovieBloc;
+    },
+    act: (bloc) => bloc.add(FetchTopRatedMovie()),
+    wait: const Duration(milliseconds: 100),
+    expect: () => [
+      TopRatedMovieLoading(),
+      TopRatedMovieError('Server Failure'),
+    ],
+    verify: (bloc) {
+      verify(mockGetTopRatedMovies.execute());
+    },
+  );
+}
